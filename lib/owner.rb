d737c34b0bce67fc1fcdd8947e0d601a9ed1430f
Chayan Martinez <nayachmartinez68@gmail.com>
2020-02-14 06:25:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,36 +1,19 @@
+
 class Owner
-  
-  @@all = []
-  @@count = 0
-  
-  attr_reader :owner, :species
-  
-  def initialize(owner, species = "human")
-    @owner = owner
+  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@owners = []
+
+  def initialize(species, pets = {:fishes => [], :dogs => [], :cats => []})
     @species = species
-    @@all << self
-    @@count += 1
+    @pets = pets
+    @@owners << self
   end
-  
+
   def say_species
-    "I am a human."
+    "I am a #{@species}."
   end
-  
-  def self.all
-    @all
-  end
-  
-  def self.count
-    @@count
-  end
-  
-  def self.reset_all
-    self.all.clear
-  end
-  
-  def name=(name)
-    @name = name
-  end
-    
-    
+
 end
