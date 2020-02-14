ef7d7f4bba6f81a40fd0e37770d9cbe6bf48b163
Chayan Martinez <nayachmartinez68@gmail.com>
2020-02-14 06:38:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-  # code goes here
+  
+  @@dogs = []
+  attr_accessor :name, :owner, :mood
+  
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @@cats << self
+  end
+  
+  def dogs
+    @@dogs
+  end
+  
 end
