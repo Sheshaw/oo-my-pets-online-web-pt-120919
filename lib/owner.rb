a1113da236f63169061b4a977589de9a57c1161b
Chayan Martinez <nayachmartinez68@gmail.com>
2020-02-14 06:11:02 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,15 +1,23 @@
 class Owner
   
+  @@all = []
   attr_reader :name, :species
   
   def initialize(name, species = "human")
     @name = name
     @species = species
+    @@all << self
   end
   
   def say_species
     "I am a human."
   end
+  
+  def self.all
+    @all
+  end
+    
+  
   
   
 end
