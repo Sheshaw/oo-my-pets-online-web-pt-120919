b07f748d5d92aeadffc695e2cbe801d1a64e20dd
Chayan Martinez <nayachmartinez68@gmail.com>
2020-02-14 06:37:39 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,15 @@
 class Cat
   
+  @@cats = []
   attr_accessor :name, :owner, :mood
   
-  def initialize(name, owner)
+  def initialize(name, owner = )
     @name = name
     @mood = "nervous"
+    @@cats << self
+  end
+  
+  def cats
+    @@cats
   end
 end
